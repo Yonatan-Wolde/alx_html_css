7cd08f891f16149e5e67c0ba8c429698b2eb865d
Yonatan Wolde <yonatanwolde251@gmail.com>
2025-08-08 14:18:04 +0300

    correct @media and animation

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -41,3 +41,75 @@ sections.forEach(section => observer.observe(section));
 
 
 
+
+
+//mobile and tablet touch
+
+const whatwedoitems = document.querySelectorAll('.whatwedoitem');
+const ourresultsitems = document.querySelectorAll('.ourresultsitem');
+
+
+    // for .whatwedoitem
+    whatwedoitems.forEach(whatwedoitem => {
+      // Mobile tap
+      whatwedoitem.addEventListener('touchstart', () => {
+        whatwedoitem.classList.add('whatwedoanimate');
+        setTimeout(() => whatwedoitem.classList.remove('whatwedoanimate'), 500);
+      });
+
+      //mouse hover
+      whatwedoitem.addEventListener('mouseenter', () => {
+        whatwedoitem.classList.add('whatwedoanimate');
+        setTimeout(() => whatwedoitem.classList.remove('whatwedoanimate'), 500);
+      });
+
+      // Optional: also respond to clicks on desktop
+      whatwedoitem.addEventListener('click', () => {
+        whatwedoitem.classList.add('spin');
+        setTimeout(() => whatwedoitem.classList.remove('spin'), 500);
+      });
+    });
+
+ 
+    // for .ourresultsitem
+    ourresultsitems.forEach(ourresultsitem => {
+      // Mobile tap
+      ourresultsitem.addEventListener('touchstart', () => {
+        ourresultsitem.classList.add('ourresultsanimate');
+        setTimeout(() => ourresultsitem.classList.remove('ourresultsanimate'), 500);
+      });
+
+      //mouse hover
+      ourresultsitem.addEventListener('mouseenter', () => {
+        ourresultsitem.classList.add('ourresultsanimate');
+        setTimeout(() => ourresultsitem.classList.remove('ourresultsanimate'), 500);
+      });
+
+      //clicks on desktop
+      ourresultsitem.addEventListener('click', () => {
+        ourresultsitem.classList.add('spin');
+        setTimeout(() => ourresultsitem.classList.remove('spin'), 500);
+      });
+    });
+
+
+
+
+
+//simplified version of the animation
+
+// const whatwedoitems = document.querySelectorAll('.whatwedoitem');
+
+
+// function triggerAnimation(element) {
+//   element.classList.add('whatwedoanimate');
+//   setTimeout(() => {element.classList.remove('whatwedoanimate')}, 500)
+// }
+
+
+// whatwedoitems.forEach(item => {
+//   item.addEventListener('touchstart', triggerAnimation(item))
+//     item.addEventListener('mouseenter', triggerAnimation(item))
+//       item.addEventListener('click', triggerAnimation(item))
+// });
+
